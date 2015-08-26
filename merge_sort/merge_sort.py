4067a48abe9f9ec56c6fbfcbcc573b7677419394
Jorge Yau <jyau01@citymail.cuny.edu>
2015-08-26 08:36:54 -0400

    Added more efficient merge sort with indices

--- merge_sort/merge_sort.py
+++ merge_sort/merge_sort.py
@@ -6,10 +6,6 @@ Conceptually, merge sort works as follows:
    (a list of 1 element is considered sorted).
 2. Repeatedly merge sublists to produce new sorted sublists until
    there is only 1 sublist remaining. This will be the sorted list.
-
-Notes:
-You can probably optimize this solution by replacing the list pops
-with indices. It will be an exercise for you! :)
 """
 
 def sort(array=None):
@@ -20,22 +16,50 @@ def sort(array=None):
 
     # Divide array.
     half_of_length = length / 2
-    sorted_left = sort(array[0:half_of_length])
-    sorted_right = sort(array[half_of_length:])
+    left_array = sort(array[0:half_of_length])
+    right_array = sort(array[half_of_length:])
     merged_array = []
 
     # Perform merge with comparisons as long as both arrays are not empty.
-    while sorted_left and sorted_right:
-        left_element = sorted_left[0]
-        right_element = sorted_right[0]
-        if left_element <= right_element:
-            merged_array.append(sorted_left.pop(0))
+    while left_array and right_array:
+        if left_array[0] <= right_array[0]:
+            merged_array.append(left_array.pop(0))
         else:
-            merged_array.append(sorted_right.pop(0))
+            merged_array.append(right_array.pop(0))
 
     # Perform the rest of the merge for the remaining array.
-    while sorted_left:
-        merged_array.append(sorted_left.pop(0))
-    while sorted_right:
-        merged_array.append(sorted_right.pop(0))
+    while left_array:
+        merged_array.append(left_array.pop(0))
+    while right_array:
+        merged_array.append(right_array.pop(0))
     return merged_array
+
+
+def sort_efficiently(array=None):
+    # Base Case.
+    length = len(array)
+    if length < 2:
+        return array
+
+    # Divide Array.
+    half_of_length = length / 2
+    left_array = sort(array[0:half_of_length])
+    right_array = sort(array[half_of_length:])
+    merged_array = []
+
+    # Perform merge with indices.
+    left_index, right_index = 0, 0
+    left_length, right_length = len(left_array), len(right_array)
+
+    while left_index < left_length and right_index < right_length:
+        left_element = left_array[left_index]
+        right_element = right_array[right_index]
+        if left_element <= right_element:
+            merged_array.append(left_element)
+            left_index += 1
+        else:
+            merged_array.append(right_element)
+            right_index += 1
+
+    # Concat the merged array with the rest of the unvisited elements.
+    return merged_array + left_array[left_index:] + right_array[right_index:]
